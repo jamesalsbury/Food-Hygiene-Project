24e66eae3d55a03e77260771de8081697920cd5b
James Salsbury (UG) <b7009283@newcastle.ac.uk>
2020-11-30 14:10:07 +0000

    Adding shapefile

--- R/shapefile.R
+++ R/shapefile.R
@@ -4,6 +4,8 @@ library(tidyverse)
 library(mapview)
 library(leaflet)
 library(maptools)
+library(rgeos)
+library(rgdal)
 
 full_postcode_dep_data <- readRDS("data/full_postcode_dep_data.rds")
 my_establishment_data <- readRDS("data/my_establishment_data.rds")
@@ -19,68 +21,29 @@ for (i in 33:83){
   establishment_dep_merged[,i] <- as.numeric(as.character(establishment_dep_merged[,i]))
 }
 
+newcastle_box <- readRDS("data/Newcastle_points.rds")
 
-newcastle_boundary <- sf::read_sf("/Users/jamesalsbury/Downloads/Download_VectorTunnel_1626336/vml_3786818/nz/vml-nz26nw_Road_Centreline.shp") %>%
-  sf::st_transform('+proj=longlat +datum=WGS84')
-
-
-newcastle_boundary1 <- sf::read_sf("/Users/jamesalsbury/Downloads/Download_VectorTunnel_1626336/vml_3786818/nz/vml-nz26ne_Road_Centreline.shp") %>%
-  sf::st_transform('+proj=longlat +datum=WGS84')
-
-newcastle_boundary2 <- sf::read_sf("/Users/jamesalsbury/Downloads/Download_VectorTunnel_1626336/vml_3786818/nz/vml-nz26se_Road_Centreline.shp") %>%
-  sf::st_transform('+proj=longlat +datum=WGS84')
-
-newcastle_boundary3 <- sf::read_sf("/Users/jamesalsbury/Downloads/Download_VectorTunnel_1626336/vml_3786818/nz/vml-nz26sw_Road_Centreline.shp") %>%
-  sf::st_transform('+proj=longlat +datum=WGS84')
-
-newcastle_box <- establishment_dep_merged %>%
-  filter(long>-1.69321) %>%
-  filter(long < -1.53239) %>%
-  filter(lat > 54.93259) %>%
-  filter(lat < 55.02451)
+newcastle_roads <- readRDS("data/Newcastle_roads.rds")
 
 box_sp <- SpatialPoints(coords = c(newcastle_box[10], newcastle_box[11]))
 
-new <- rbind(newcastle_boundary, newcastle_boundary1)
-new <- rbind(new, newcastle_boundary2)
-new <- rbind(new, newcastle_boundary3)
-new1 <- st_sfc(new$geometry, crs = "+proj=longlat +datum=WGS84")
-new1 <- as(new1, "Spatial")
-combined <- snapPointsToLines(box_sp, new1)
 
+new1 <- st_sfc(newcastle_roads$geometry, crs = "+proj=longlat +datum=WGS84")
+new1 <- as(newcastle_roads, "Spatial")
+combined <- snapPointsToLines(box_sp, new1, idField = "Fid")
 
-mapview(new)
 road_count <- combined@data %>%
   count(nearest_line_id)
 
-merged_count <- merge(new, road_count, by.x = )
 
-?merge()
+merged_snapped <- merge(new1@data, road_count, by.x = "Fid", by.y = "nearest_line_id")
 
-road_count
+n <- vector(length=nrow(newcastle_roads), mode="numeric")
 
-new %>%
-  filter(FeatCode=="ID18588")
-
-
-mycount <- combined@data %>%
-  count(nearest_line_id)
+newcastle_roads <- newcastle_roads %>%
+  add_column(n)
 
 
-leaflet() %>%
-  addTiles() %>%
-  addMarkers(lng = combined@coords[,1], lat = combined@coords[,2]) %>%
-  leaflet(new1) %>%
-  addPolylines()
-
-leaflet(new1) %>%
-  addTiles() %>%
-  addPolylines() %>%
-  addMarkers(lng = combined@coords[,1], lat = combined@coords[,2])
+merged_snapped <- st_join(newcastle_roads, merged_snapped)
 
 
-class(new1@lines)
-
-
-mycount$nearest_line_id %>%
-  arrange()
